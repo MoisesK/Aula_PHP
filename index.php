6cf4bcf578b194b86c9393827b85b0da43654ad3
MoisesK <moises979@gmail.com>
2020-11-13 17:38:11 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+
+//print_r($categorias);
+
+$nome = 'Moises'
+$idade = 20;
+
+var_dump($nome);
+var_dump($idade)
